ddf8f80c2c5e79615250873f8864e82d632f736c
pcatbaso <pcat@baso.com.mx>
2023-04-12 11:57:30 -0600

    Primera versión

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoneyGoalComponent } from './components/money-goal/money-goal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/MoneyGoal', pathMatch: 'full'},
+  {path: 'MoneyGoal', component: MoneyGoalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/money-goal/money-goal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-money-goal',
+  templateUrl: './money-goal.component.html',
+  styleUrls: ['./money-goal.component.scss']
+})
+export class MoneyGoalComponent {
+
+}
